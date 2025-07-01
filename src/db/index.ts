eb7ce0dd8c542699de8c5e7137cb7f7c2770c99f
Ján Korček <jan.korcek@goodrequest.com>
2025-07-01 13:36:37 +0200

    TECH-988: remove formatting

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,41 +1,36 @@
-import fs from 'fs';
-import { Sequelize } from 'sequelize';
+import fs from 'fs'
+import { Sequelize } from 'sequelize'
 
-import defineExercise from './exercise';
-import defineProgram from './program';
+import defineExercise from './exercise'
+import defineProgram from './program'
 
-const sequelize: Sequelize = new Sequelize(
-  'postgresql://postgres:root@localhost:5432/fitness_app',
-  {
-    logging: false,
-  }
-);
+const sequelize: Sequelize = new Sequelize('postgresql://postgres:root@localhost:5432/fitness_app', {
+	logging: false
+})
 
-sequelize
-  .authenticate()
-  .catch((e: any) => console.error(`Unable to connect to the database${e}.`));
+sequelize.authenticate().catch((e: any) => console.error(`Unable to connect to the database${e}.`))
 
-const Exercise = defineExercise(sequelize, 'exercise');
-const Program = defineProgram(sequelize, 'program');
+const Exercise = defineExercise(sequelize, 'exercise')
+const Program = defineProgram(sequelize, 'program')
 
 const models = {
-  Exercise,
-  Program,
-};
-type Models = typeof models;
+	Exercise,
+	Program
+}
+type Models = typeof models
 
 // check if every model is imported
-const modelsFiles = fs.readdirSync(__dirname);
+const modelsFiles = fs.readdirSync(__dirname)
 // -1 because index.ts can not be counted
-if (Object.keys(models).length !== modelsFiles.length - 1) {
-  throw new Error('You probably forgot import database model!');
+if (Object.keys(models).length !== (modelsFiles.length - 1)) {
+	throw new Error('You probably forgot import database model!')
 }
 
 Object.values(models).forEach((value: any) => {
-  if (value.associate) {
-    value.associate(models);
-  }
-});
+	if (value.associate) {
+		value.associate(models)
+	}
+})
 
-export { models, sequelize };
-export type { Models };
+export { models, sequelize }
+export type { Models }
